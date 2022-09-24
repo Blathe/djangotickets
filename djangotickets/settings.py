c2d11d7fbaba4aaf151f392ff9433365cccc0996
Scott Peterts <Scottpeters2281@gmail.com>
2022-09-23 20:34:50 -0700

    switching to postgresql for heroku

--- djangotickets/settings.py
+++ djangotickets/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os.path
 
+import dj_database_url
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -85,12 +87,16 @@ WSGI_APPLICATION = 'djangotickets.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
