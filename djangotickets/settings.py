2fbbd49e768a3d9fb6c5c6056c80d5ddaf991333
Scott Peterts <Scottpeters2281@gmail.com>
2022-09-23 20:21:05 -0700

    deploy

--- djangotickets/settings.py
+++ djangotickets/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ( os.path.join('static'), )
+#STATICFILES_DIRS = ( os.path.join('static'), )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
