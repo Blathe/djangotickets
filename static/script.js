8ac2388339d7cdaabfbedd80855260fb27ff5452
Scott Peters <Scottpeters2281@gmail.com>
2022-10-02 11:49:03 -0700

    fixed issue #3 Hide Closed Tickets

--- static/script.js
+++ static/script.js
@@ -1,26 +1,38 @@
 const urlParams = new URLSearchParams(window.location.search);
 
 var hideClosedFilterCheckbox = document.getElementById('hide-closed-filter');
-hideClosedFilterCheckbox.checked = localStorage.getItem('hideClosedChecked');
-
 var sortSelectionBox = document.getElementById('sort-select-box');
-sortSelectionBox.value = localStorage.getItem('sortSelection');
+
+setupSessions()
+
+function setupSessions(){
+    if (urlParams.has('filters')){
+        hideClosedFilterCheckbox.checked = sessionStorage.getItem('hideClosedChecked');
+    } else {
+        sessionStorage.removeItem('hideClosedChecked');
+    }
+    if (urlParams.has('sort')){
+        sortSelectionBox.value = sessionStorage.getItem('sortSelection');
+    } else {
+        sessionStorage.removeItem('sortSelection');
+    }
+}
 
 function updateSort() {
     var sortSelection = sortSelectionBox.value;
     urlParams.set('sort', sortSelection);
-    localStorage.setItem('sortSelection', sortSelection)
+    sessionStorage.setItem('sortSelection', sortSelection)
     window.location.search = urlParams;
 }
 
 function handleHideClosedTickets(checkbox) {
     if (checkbox.checked){
-        localStorage.setItem('hideClosedChecked', true);
+        sessionStorage.setItem('hideClosedChecked', true);
         urlParams.set('filters', 'hideClosed')
         window.location.search = urlParams;
     } else {
         urlParams.delete('filters')
-        localStorage.removeItem('hideClosedChecked')
+        sessionStorage.removeItem('hideClosedChecked')
         window.location.search = urlParams;
     }
 }
