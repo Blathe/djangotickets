424b4a22f673be1ebddd9389d6b246218ed28b1f
Scott Peters <scottpeters2281@gmail.com>
2023-04-21 12:14:27 -0700

    changing from auto_now to auto_now_add - auto_now causes the creation_date to change every time the model is changed.

--- tickets/models.py
+++ tickets/models.py
@@ -16,7 +16,7 @@ class Ticket(models.Model):
 
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=600)
-    creation_date = models.DateTimeField(auto_now=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
     closed_date = models.DateTimeField(blank=True, null=True)
     owner = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     status = models.CharField(choices=STATUS_CHOICES, max_length=20, default="OPEN")
