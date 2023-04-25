7d5067ca67bea72b012b28d333cb7e3b20402616
Scott Peters <scottpeters2281@gmail.com>
2023-04-25 13:12:40 -0700

    added basic report UI, and testing for csv downloads

--- reports/urls.py
+++ reports/urls.py
@@ -5,4 +5,5 @@ from . import views
 # /reports*
 urlpatterns = [
     path('', views.index, name="index"),
+    path('test', views.csv_test, name="csv"),
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 
+import csv
+
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
+
+from django.http import HttpResponse
 
 # Create your views here.
 
 @login_required
 def index(request):
     return render(request, "reports/index.html")
+
+def csv_test(request):
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(
+        content_type="text/csv",
+        headers={"Content-Disposition": 'attachment; filename="somefilename.csv"'},
+    )
+
+    writer = csv.writer(response)
+    writer.writerow(["First row", "Foo", "Bar", "Baz"])
+    writer.writerow(["Second row", "A", "B", "C", '"Testing"', "Here's a quote"])
+
+    return response
