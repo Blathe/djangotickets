d235fda8e32a6ca1bb8aab275106e6a261328fbc
Scott Peterts <Scottpeters2281@gmail.com>
2023-04-25 21:59:34 -0700

    big changes to the reporting functionality

--- reports/views.py
+++ reports/views.py
@@ -1,20 +1,55 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect
-from django.contrib import messages
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login
 from django.core.paginator import Paginator
+
+from django.utils.timezone import now
 
 import csv
 import datetime
 
+from tickets.models import Ticket
+
+from .forms import ReportGenerationForm
 
 # Create your views here.
 
 @login_required
 def index(request):
-    return render(request, "reports/index.html")
-
+    if request.method == "POST":
+        form = ReportGenerationForm(request.POST)
+        
+        if form.is_valid():
+            
+            user = form.cleaned_data['user']
+            status = form.cleaned_data['status']
+            group_by = form.cleaned_data['group_by']
+            time = form.cleaned_data['time']
+            
+            if user is None:
+                all_tickets = Ticket.objects.all()
+            else:
+                all_tickets = Ticket.objects.filter(owner = user)
+                
+            if status != "ANY":
+                all_tickets = all_tickets.filter(status = status)
+            
+            if group_by == "PRIORITY":
+                all_tickets = all_tickets.order_by("priority")
+            elif group_by == "USER":
+                all_tickets = all_tickets.order_by("owner")
+            elif group_by == "STATUS":
+                all_tickets = all_tickets.order_by("status")
+                
+            paginator = Paginator(all_tickets, 100)
+            page_number = request.GET.get('page')
+            page_obj = paginator.get_page(page_number)
+            return render(request, "reports/index.html", {"form": form, 'page_obj': page_obj})
+    
+    else:
+        form = ReportGenerationForm()
+        return render(request, "reports/index.html", {"form": form})
+    
 def csv_test(request):
     # Create the HttpResponse object with the appropriate CSV header.
     file_name = "report-" + datetime.now
