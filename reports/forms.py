c921dfa082bb78b95d1c497bee587e3597b26b0f
Scott Peters <scottpeters2281@gmail.com>
2023-04-26 19:59:36 -0700

    reporting and csv functionality

--- reports/forms.py
+++ reports/forms.py
@@ -27,10 +27,14 @@ class ReportGenerationForm(forms.Form):
         ('PRIORITY', 'Priority')
     )
       
-    user = UserChoiceField(queryset=User.objects.filter(is_superuser=False), empty_label="Any", required=False, label="Select User:")
-    status = forms.ChoiceField(choices = STATUS_CHOICES, required=False, label="Ticket Status:")
-    time = forms.ChoiceField(choices = TIME_CHOICES, initial='ALL TIME', label="Date Created:")
-    group_by = forms.ChoiceField(choices = GROUP_BY_CHOICES, initial='PRIORITY', label="Group By:")
+    user = UserChoiceField(queryset=User.objects.filter(is_superuser=False), empty_label="Any", required=False, label="Select User:",
+    widget=forms.Select(attrs={'class': 'form-select'}))
+    status = forms.ChoiceField(choices = STATUS_CHOICES, required=False, label="Ticket Status:",
+    widget=forms.Select(attrs={'class': 'form-select'}))
+    time = forms.ChoiceField(choices = TIME_CHOICES, initial='ALL TIME', label="Date Created:",
+    widget=forms.Select(attrs={'class': 'form-select'}))
+    group_by = forms.ChoiceField(choices = GROUP_BY_CHOICES, initial='PRIORITY', label="Group By:",
+    widget=forms.Select(attrs={'class': 'form-select'}))
     
     
     
